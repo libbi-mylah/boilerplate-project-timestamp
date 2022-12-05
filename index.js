20e8d8a23de3d738609069f5dca5493b810aef1a
Libbi Mylah <95930674+libbi-mylah@users.noreply.github.com>
2022-12-05 15:10:43 -0500

    competed code for project

--- index.js
+++ index.js
@@ -1,32 +1,44 @@
 // index.js
 // where your node app starts
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
 
-// init project
-var express = require('express');
-var app = express();
+const cors = require("cors");
+app.use(cors({ optionSuccessStatus: 200 }));
+app.use(bodyParser.json());
 
-// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
-var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(express.static("public"));
 
-// http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
-
-// http://expressjs.com/en/starter/basic-routing.html
+// index
 app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+  res.sendFile(__dirname + "/views/index.html");
 });
 
+// timestamp API
+app.get("/api/:date?", (req, res) => {
+  const dateInput = req.params.date;
+  let date;
 
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  // checking if no date input is provided in the api
+  if (!dateInput) {
+    date = new Date();
+  } else {
+    // if string date is multiplied by 1, it will give NaN, while number will return number. So, checking for unix number
+    const checkUnix = dateInput * 1;
+    date = isNaN(checkUnix) ? new Date(dateInput) : new Date(checkUnix);
+  }
+
+  //check if date input format is valid
+  if (date == "Invalid Date") {
+    res.json({ error: "Invalid Date" });
+  } else {
+    const unix = date.getTime();
+    const utc = date.toUTCString();
+    res.json({ unix, utc });
+  }
 });
 
-
-
-// listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+var listener = app.listen(process.env.PORT || 5500, function () {
+  console.log("Your app is listening on port " + listener.address().port);
 });
